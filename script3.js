6f83f5a5315ecc6afd54acdfcf35e2fd47102602
AM <mail@mail.ru>
2021-05-19 16:59:52 +0300

    omg

--- script3.js
+++ script3.js
@@ -1,22 +1,22 @@
 document.write('Задача №1 - 3 урок <br>');
 // 1. С помощью цикла while вывести все простые числа в промежутке от 0 до 100.
 
-var num = 8;
-function pizdec(num) {
-// цикл
-while (num <= 100) {
-   var i = 2;
-   // цикл 
-   while (i <= num / 2) {
-      // условие
-      if (num % i !== 0) {
-         document.write(num + '<br>');
-      }
-      i++;
-   }
-   num++;
-}
-}
+// var num = 8;
+// function pizdec(num) {
+// // цикл
+// while (num <= 100) {
+//    var i = 2;
+//    // цикл 
+//    while (i <= num / 2) {
+//       // условие
+//       if (num % i !== 0) {
+//          document.write(num + '<br>');
+//       }
+//       i++;
+//    }
+//    num++;
+// }
+// }
 
 
 
@@ -46,41 +46,42 @@ while (num <= 100) {
 // b) Организовать функцию countBasketPrice, которая будет считать стоимость корзины.
 document.write('Задача №2,3 - 3 урок <br>');
 
-// var arrCart = [
-//    {
-//       title: 'product1',
-//       price: 300,
-//       quantity: 1
-//    },
-//    {
-//       title: 'product2',
-//       price: 300,
-//       quantity: 1
-//    },
-//    {
-//       title: 'product13',
-//       price: 600,
-//       quantity: 2
-//    }
-// ];
+var arrCart = [
+   {
+      title: 'product1',
+      price: 300,
+      quantity: 1
+   },
+   {
+      title: 'product2',
+      price: 300,
+      quantity: 1
+   },
+   {
+      title: 'product13',
+      price: 600,
+      quantity: 2
+   }
+];
 
-// var sum = 0;
-// function countBasketPrice() {
-//    for (var item of arrCart) {
-//       sum += item.price * item.quantity;
-//    }
-//    console.log(`Общая сумма корзины составляет - ${sum}`);
-// }
-// countBasketPrice();
+var sum = 0;
+function countBasketPrice() {
+   for (var item of arrCart) {
+      sum += item.price * item.quantity;
+   }
+   document.write(`Общая сумма корзины составляет - ${sum}`);
+}
+countBasketPrice();
 
 
 
 
 // 4.*Вывести с помощью цикла for числа от 0 до 9, не используя тело цикла. Выглядеть это должно так:
 // for(…){// здесь пусто}
-document.write('Задача №4 - 3 урок <br>');
-// for (let i = 0; i < 10; console.log(i++)) {
-// }
+document.write('<br>Задача №4 - 3 урок <br>');
+
+for (let i = 0; i < 10;  document.write(i++)) {
+}
 
 // 5. *Нарисовать пирамиду с помощью console.log, как показано на рисунке, только у вашей пирамиды должно быть 20 рядов, а не 5:
 // x
@@ -89,14 +90,15 @@ document.write('Задача №4 - 3 урок <br>');
 // xxxx
 // xxxxx
 
-document.write('Задача №5 - 3 урок <br>');
+document.write('<br> Задача №5 - 3 урок <br>');
 
-// var pyramid = '';
-// var per = 'x';
-// var l = 0;
+var pyramid = '';
+var per = 'x';
+var l = 0;
 
-// while (l < 20) {
-//    l++;
-//    pyramid += per;
-//    document.write(l + ' ' + pyramid + '<br>');
-// }
+while (l < 20) {
+   l++;
+   pyramid += per;
+   document.write(pyramid + '<br>');
+}
+
