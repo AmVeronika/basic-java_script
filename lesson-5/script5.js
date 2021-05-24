c1be119c0279b14483eb4100da8be909bbae552f
AM <mail@mail.ru>
2021-05-24 18:45:38 +0300

    homework5

--- lesson-5/script5.js
+++ lesson-5/script5.js
@@ -3,23 +3,31 @@ var сhessBoard = document.createElement('div') // шахматная доска
 сhessBoard.classList.add('сhess-board'); // подключила стили к шахматной доске
 document.body.append(сhessBoard); //Вставила в body стили шахматной доски
 
-var line; // Переменная строка клеток
 // line.classList.add('line');
 var column; //Переменная столбец клеток
 // column.classList.add('column')
 
-cell();
+insertСells();
 
-function cell() {
-   for (var i = 1; i <= 8; i++) {
-      line = document.createElement('p');
-      сhessBoard.append(line);
-      line.classList.add('line');
+function insertСells() {
+   for (var j = 0; j < 8; j++) {
+      for (var i = 0; i < 8; i++) {
+         var cell = document.createElement('p'); // клетка
+         cell.innerText = 'WTF?';
+         сhessBoard.append(cell);
+         cell.classList.add('cell');
+      }
    }
+};
+var cells = document.querySelectorAll('.cell'); // Переменная массив клеток
 
+for (var i = 0; i < cells.length; i++) {
+   if (i % 2 == 0) {
+      cells[i].style.background = "rgb(111, 53, 0)";
+
+   } 
+   console.log(i);
 }
-for (var j = 1; j <= 8; j++) {
-  cell();
 
-}
 
+console.log(cells);
